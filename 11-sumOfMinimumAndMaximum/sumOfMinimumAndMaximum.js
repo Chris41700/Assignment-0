6f6148618d2a8bc4f77cc9f79c390e0c0774cc4a
Chris41700 <ChrisHui417@gmail.com>
2021-12-29 05:42:26 -0500

    sumOfMinimumAndMaximum Function Updated

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -2,7 +2,7 @@ function sumOfMinimumAndMaximum(nums) {
     let sum = 0;
 
     for (var i = 0; i < nums.length; i++) {
-        if (i === 0 || i == nums.length - 1)
+        if (i === 0 || i === nums.length - 1)
             sum += nums[i];
     }
 
