ab4cf26a007015aaa2f56867c21ed3bc29cbff5c
Chris41700 <ChrisHui417@gmail.com>
2021-12-29 15:00:06 -0500

    productOfAnyNumbers Function Updated

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+    let product = 1;
+
+    for (let i of args)
+        product *= i;
+
+    return product;
 }
 
 // Do not edit this line;
