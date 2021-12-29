5cc6b3e9add2a972c333b0f31bdc08c6495cb00f
Chris41700 <ChrisHui417@gmail.com>
2021-12-29 05:20:23 -0500

    countOfAllNumbersSmallerThanTarget Function Updated

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+    let counter = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === true || arr[i] === false || arr[i].startsWith(""))
+            counter++;
+    }
+
+    return counter;
 }
 
 // Do not edit this line;
