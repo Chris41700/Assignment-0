7ff3fa64abf17ef025f2ea46994bbce132c09e1a
Chris41700 <ChrisHui417@gmail.com>
2021-12-29 06:19:22 -0500

    fizzBuzz Function Updated

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,21 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+    let multiOfThree = "Fizz";
+    let multiOfFive = "Buzz";
+    let multiofThreeAndFive = "FizzBuzz";
+    let arr = [];
+
+    for (start; start <= end; start++) {
+        if (start % 3 === 0 && start % 5 === 0)
+            arr.push(multiofThreeAndFive);
+        else if (start % 3 === 0)
+            arr.push(multiOfThree);
+        else if (start % 5 === 0)
+            arr.push(multiOfFive)
+        else
+            arr.push(start);
+    }
+
+    return arr;
 }
 
 // Do not edit this line;
