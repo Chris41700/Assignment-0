05bc9b0d526360160c8b4ceb82c8864b48b168c0
Chris41700 <ChrisHui417@gmail.com>
2021-12-29 05:31:34 -0500

    sumOfAllOddNumbers Function Updated

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+    let oddCounter = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 == 1)
+            oddCounter++;
+    }
+
+    return oddCounter;
 }
 
 // Do not edit this line;
