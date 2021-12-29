fefa4914e7cdc39fa6537844b4aeeb0b7cad44b9
Chris41700 <ChrisHui417@gmail.com>
2021-12-29 04:18:35 -0500

    titleCaseEdit Function Updated

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,11 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+    const capitalizedWord = title.toLowerCase().split(" ");
+
+    for (var i = 0; i < capitalizedWord.length; i++) {
+        capitalizedWord[i] = capitalizedWord[i].replace(capitalizedWord[i].charAt(0), capitalizedWord[i].charAt(0).toUpperCase());
+    }
+
+    return capitalizedWord.join(' ');
 }
 
 // Do not edit this line;
