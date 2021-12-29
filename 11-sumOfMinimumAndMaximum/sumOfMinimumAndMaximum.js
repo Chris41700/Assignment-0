ff215e2269cffd94c6226da1b4c510126db6dcea
Chris41700 <ChrisHui417@gmail.com>
2021-12-29 05:41:30 -0500

    sumOfMinimumAndMaximum

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,12 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+    let sum = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+        if (i === 0 || i == nums.length - 1)
+            sum += nums[i];
+    }
+
+    return sum;
 }
 
 // Do not edit this line;
