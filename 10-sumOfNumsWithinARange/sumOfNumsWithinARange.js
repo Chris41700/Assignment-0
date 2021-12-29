591f40598f5c496ce8261f04aaef76670056314a
Chris41700 <ChrisHui417@gmail.com>
2021-12-29 05:35:40 -0500

    sumOfNumsWithinARange Function Updated

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,12 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+    let inRange = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+        if (start <= nums[i] && nums[i] <= end)
+            inRange++;
+    }
+
+    return inRange;
 }
 
 // Do not edit this line;
