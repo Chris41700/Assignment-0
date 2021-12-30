10c35b33e6748c7289bd5fb64e1c35b5866e7da9
Chris41700 <ChrisHui417@gmail.com>
2021-12-30 10:29:04 -0500

    frequencyCounter Function Updated

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,14 @@
 function frequencyCounter(word) {
-  // Insert code here;
+    let freq = {};
+
+    for (var i = 0; i < word.length; i++) {
+        if (freq[word.charAt(i)])
+            freq[word.charAt(i)]++;
+        else
+            freq[word.charAt(i)] = 1;
+    }
+
+    return freq;
 }
 
 // Do not edit this line;
